4b171a32e7ef14fa0d7f2617d42a3b5da7bfa9a6
Anna Sippel <asippel129@gmail.com>
2019-01-31 21:01:48 -0700

    trying to get heroku to work

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-    id int NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name varchar(225) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
